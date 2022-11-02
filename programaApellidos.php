da1c538a29b95a5519ab503335d5a996c70fd38d
Romartinez97 <romartinez1997@yahoo.com.ar>
2022-11-02 18:50:14 -0300

    Agregadas las palabras nuevas al código y actualizado el Excel

--- programaApellidos.php
+++ programaApellidos.php
@@ -23,8 +23,8 @@ function cargarColeccionPalabras()
     $coleccionPalabras = [
         "MUJER", "QUESO", "FUEGO", "CASAS", "RASGO",
         "GATOS", "GOTAS", "HUEVO", "TINTO", "NAVES",
-        "VERDE", "MELON", "YUYOS", "PIANO", "PISOS"
-        /* ... COMPLETAR ... Agregar 5 palabras más */
+        "VERDE", "MELON", "YUYOS", "PIANO", "PISOS",
+        "ACERO", "BALDE", "TAZAS", "JAULA", "ZORRO"
     ];
 
     return ($coleccionPalabras);
